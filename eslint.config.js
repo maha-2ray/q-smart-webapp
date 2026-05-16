343b5440850866f48404328abff50d41048255e6
Muhammed L. Touray <touraymuhammed2000@gmail.com>
2026-05-16 00:38:12 +0000

    fix: fix typescript errors

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,9 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -17,6 +17,9 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.app.json", "./tsconfig.test.json"],
+      },
     },
   },
-])
+]);
